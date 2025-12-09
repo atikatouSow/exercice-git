e5ed487f1ac17ddaac0bf4416693d825e3b0a354
atikatouSow <sowatikatou@gmail.com>
2025-12-09 13:22:08 -0500

    Create test.cs
    Ajout dossier exercice + fichier test

---
+++ exercice/test.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bonjour, ceci est mon premier test avec GitHub !");
+        
+        // Appel d'une petite fonction
+        DireBonjour("Atikatou");
+    }
+
+    static void DireBonjour(string nom)
+    {
+        Console.WriteLine("Bonjour " + nom + " !");
+    }
+}
